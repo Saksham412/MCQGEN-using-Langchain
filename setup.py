024e3b2d5afea369773403f3486fa028e8827f75
Saksham <sakshamshinde412@gmail.com>
2024-05-17 19:09:16 +0530

    mcq experiment update

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 # To install local package in virtual enviornment we create this file
+
+from setuptools import find_packages, setup
+
+setup(
+    name="mcqgen",
+    version="0.0.1",
+    author="saksham shinde",
+    author_email="sakshamshinde412@gmail.com",
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
